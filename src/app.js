e035abc8ae9796130c0162a98d497cb6aaef0f73
moncherry369 <ssgarrido1994@gmail.com>
2023-01-17 08:37:32 +0000

    Final edit to correct style css integration

--- src/app.js
+++ src/app.js
@@ -5,42 +5,38 @@ function generateExcuse() {
     "The milkman ",
     "My grandma with dementia ",
     "The kid who thinks hes a ninja turtle ",
-    "Three squirles and a raccoon "];
+    "Three squirles and a raccoon "
+  ];
 
-    let shuffle = Math.random();
-    let totalWho = who.length;
-    let shuffleIndex = Math.floor(shuffle * totalWho);
-    let randomWho = who[shuffleIndex];
-   
+  let shuffle = Math.random();
+  let totalWho = who.length;
+  let shuffleIndex = Math.floor(shuffle * totalWho);
+  let randomWho = who[shuffleIndex];
 
-    // ACTION GENERATOR //
+  // ACTION GENERATOR //
 
-    let action = [
-      "spilled rotten milk all over ",
-      "stole ",
-      "pretended to be a sprinkler while peeing all over ",
-      "broke "];
-  
-      let shuffleAction = Math.random();
-      let totalAction = action.length;
-      let shuffleActionIndex = Math.floor(shuffleAction * totalAction);
-      let randomAction = action[shuffleActionIndex];
-      
+  let action = [
+    "spilled rotten milk all over ",
+    "stole ",
+    "pretended to be a sprinkler while peeing all over ",
+    "broke "
+  ];
 
-    // WHAT GENERATOR //
+  let shuffleAction = Math.random();
+  let totalAction = action.length;
+  let shuffleActionIndex = Math.floor(shuffleAction * totalAction);
+  let randomAction = action[shuffleActionIndex];
 
-    let what = [
-      "my dog ",
-      "my car ",
-      "me "];
-  
-      let shuffleWhat = Math.random();
-      let totalWhat = what.length;
-      let shuffleWhatIndex = Math.floor(shuffleWhat * totalWhat);
-      let randomWhat = what[shuffleWhatIndex];
-      
- 
-      // WHEN GENERATOR //
+  // WHAT GENERATOR //
+
+  let what = ["my dog ", "my car ", "me "];
+
+  let shuffleWhat = Math.random();
+  let totalWhat = what.length;
+  let shuffleWhatIndex = Math.floor(shuffleWhat * totalWhat);
+  let randomWhat = what[shuffleWhatIndex];
+
+  // WHEN GENERATOR //
 
   let when = [
     "yesterday.",
@@ -50,13 +46,12 @@ function generateExcuse() {
     "as I hopped out of my car to dance to Drake."
   ];
   let shuffleWhen = Math.random();
-      let totalWhen = when.length;
-      let shuffleWhenIndex = Math.floor(shuffleWhen * totalWhen);
-      let randomWhen = when[shuffleWhenIndex];
-      let excuseElement = document.querySelector(".excuse");
-      
-      excuseElement.innerHTML = randomWho + randomAction + randomWhat + randomWhen;
-  
+  let totalWhen = when.length;
+  let shuffleWhenIndex = Math.floor(shuffleWhen * totalWhen);
+  let randomWhen = when[shuffleWhenIndex];
+  let excuseElement = document.querySelector(".excuse");
+
+  excuseElement.innerHTML = randomWho + randomAction + randomWhat + randomWhen;
 }
 
-window.onload = generateExcuse()
+window.onload = generateExcuse();
